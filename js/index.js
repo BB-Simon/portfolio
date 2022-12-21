2c5f0f80c4d87589ab44a4c1d243b66bb2be3680
EmekaEmmanuel <emekaekeohaa@gmail.com>
2022-12-21 06:35:48 -0800

    Add validatorInput function in index.js

--- js/index.js
+++ js/index.js
@@ -2,3 +2,22 @@ const name = document.querySelector("#name")
 const email = document.querySelector("#email")
 const message = document.querySelector("#message")
 
+const inputValues = {
+  name,
+  email,
+  message
+}
+
+const validatorInput = (value) => {
+  if (name === "") {
+    console.error("name is required");
+  }
+  if (email === "") {
+    console.error("email is required");
+  }
+  if (message === "") {
+    console.error("message is required");
+  }
+} 
+
+validatorInput(inputValues)
