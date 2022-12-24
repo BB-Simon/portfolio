bcdd0e27116e36c7898a463f13a746876c3f6c7e
BB-Simon <simon.chowdery@gmail.com>
2022-12-24 17:55:19 +0200

    Add validation to contact form

--- js/index.js
+++ js/index.js
@@ -212,27 +212,26 @@ cardBtn.forEach((btn) => {
 /** *******************************************
 * Contact form validation
 ********************************************* */
+const contactForm = document.querySelector('#contact-form');
 
-const name = document.querySelector("#name")
-const email = document.querySelector("#email")
-const message = document.querySelector("#message")
-
-const inputValues = {
-  name,
-  email,
-  message
-}
-
-const validatorInput = (value) => {
-  if (name === "") {
-    console.error("name is required");
+// Validate if email is lowercase
+const validateEmail = (email) => {
+  if (/[A-Z]/.test(email)) {
+    return false;
   }
-  if (email === "") {
-    console.error("email is required");
-  }
-  if (message === "") {
-    console.error("message is required");
-  }
-} 
+  return true;
+};
 
-validatorInput(inputValues)
+// Submit contact form with email validation
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = e.target.email.value;
+  const error = document.querySelector('.error');
+  const isSubmitOk = validateEmail(email);
+  if (isSubmitOk) {
+    error.style.display = 'none';
+  } else {
+    error.style.display = 'block';
+    error.innerHTML = 'Email must content only lower case charecters';
+  }
+});
