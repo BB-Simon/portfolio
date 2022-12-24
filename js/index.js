c34cb6a6786eb39b31d6f1420c508016b8fbb044
BB-Simon <simon.chowdery@gmail.com>
2022-12-24 04:28:14 +0200

    Add dynamic data to ptoject section and add project detail modal

--- js/index.js
+++ js/index.js
@@ -27,25 +27,118 @@ listItems.forEach((item) => {
   item.addEventListener('click', closeMenuModal);
 });
 
-const seeProjectDetailsBtn = document.querySelector('#see-projects-btn');
-
+/** *******************************************
+* Prject section JS implementation
+********************************************* */
+const worksSection = document.getElementById('works');
 const projectData = [
   {
     id: 1,
-    name: 'name',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.", "A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    image: '../img/jumbo1.jpg',
-    technologies: ['Codekit', 'Github', 'Javascript', 'Bootstrap', 'Javascript', 'Bootstrap'],
-    sourceLink: 'https://github.com/emekaemmanuel/res_portfolio',
-    liveLink: 'https://github.com/emekaemmanuel/res_portfolio',
+    name: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    image: { src: '../assets/snapshoot-1.png', title: 'Portfolio' },
+    meta: ['CANOPY', 'Back End Dev', '2015'],
+    technologies: ['html', 'css', 'javascript'],
+    sourceLink: 'https://github.com/BB-Simon',
+    liveLink: 'https://bb-simon.github.io/',
+  },
+  {
+    id: 2,
+    name: 'Multi-Post <br />Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    image: { src: '../assets/snapshoot-2.png', title: 'Portfolio' },
+    meta: ['CANOPY', 'Back End Dev', '2015'],
+    technologies: ['html', 'css', 'javascript'],
+    sourceLink: 'https://github.com/BB-Simon',
+    liveLink: 'https://bb-simon.github.io/',
+  },
+  {
+    id: 3,
+    name: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    image: { src: '../assets/snapshoot-3.png', title: 'Portfolio' },
+    meta: ['CANOPY', 'Back End Dev', '2015'],
+    technologies: ['html', 'css', 'javascript'],
+    sourceLink: 'https://github.com/BB-Simon',
+    liveLink: 'https://bb-simon.github.io/',
+  },
+  {
+    id: 4,
+    name: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    image: { src: '../assets/snapshoot-4.png', title: 'Portfolio' },
+    meta: ['CANOPY', 'Back End Dev', '2015'],
+    technologies: ['html', 'css', 'javascript'],
+    sourceLink: 'https://github.com/BB-Simon',
+    liveLink: 'https://bb-simon.github.io/',
   },
 ];
 
-const openProjectDetailsModal = (e) => {
-  e.preventDefault();
-  // const body = document.body;
-  // i.e below
-  // const detailsModal = document.querySelector('details-modal');
-  // body.appendChild()
+// Listener's functions
+const createProjectCard = (project) => {
+  const cardContainer = document.createElement('div');
+  if (project.id === 1 || project.id === 3) {
+    cardContainer.classList.add('card-conntainer', 'flex-reverse');
+  } else {
+    cardContainer.classList.add('card-conntainer');
+  }
+
+  const techList = project.technologies.map((tech) => `<li class="card-category">${tech}</li>`);
+  const metaList = project.meta.map((meta, index) => {
+    if (index !== 0) {
+      return `<div class="divider"></div>
+        <span class="card-meta-item muted">${meta}</span>`;
+    }
+    return `<span class="card-meta-item">${meta}</span>`;
+  });
+
+  const html = `
+    <img src=${project.image.src} alt=${project.image.title}>
+    <div class="card-content">
+        <h2 class="card-title">${project.name}</h2>
+        <div class="card-meta">
+          ${metaList.join('')}
+        </div>
+        <p id="card-description">${project.description}</p>
+        <ul class="card-categories-wrapper">
+          ${techList.join('')}
+        </ul>
+        <div class="card-cta-wrapper">
+            <button class="card-btn see-project">See Project</button>
+        </div>
+    </div>`;
+  cardContainer.innerHTML = html;
+  worksSection.appendChild(cardContainer);
 };
-seeProjectDetailsBtn.addEventListener('click', openProjectDetailsModal);
+
+projectData.forEach((project) => createProjectCard(project));
+const cardBtn = document.querySelectorAll('.see-project');
+const modalOverlay = document.getElementById('project-detail-modal-overlay');
+const projectModal = document.getElementById('project-detail-modal');
+const projectCloseBtn = document.getElementById('project-close-btn');
+
+const openProjectModal = () => {
+  const scrollBarWidth = window.innerWidth - document.body.offsetWidth;
+  document.body.style.margin = `0px ${scrollBarWidth}px 0px 0px`;
+  document.body.style.overflow = 'hidden';
+  modalOverlay.style.display = 'block';
+  projectModal.style.display = 'block';
+};
+const closeProjectModal = () => {
+  document.body.style.margin = '';
+  document.body.style.overflow = '';
+  modalOverlay.style.display = 'none';
+  projectModal.style.display = 'none';
+};
+
+// Listeners
+cardBtn.forEach((btn) => {
+  btn.addEventListener('click', openProjectModal);
+});
+projectModal.addEventListener('click', closeProjectModal);
+projectCloseBtn.addEventListener('click', closeProjectModal);
+for (let i = 0; i < projectModal.childNodes.length; i += 1) {
+  projectModal.childNodes[i].addEventListener('click', (event) => {
+    event.stopPropagation();
+  });
+}
